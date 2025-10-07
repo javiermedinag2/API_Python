d432290765242f99eaed594febbba10ec2148c5c
Javier Medina <javiermedinag@hotmail.com>
2025-10-06 23:14:13 -0600

    Primer commit de practica

---
+++ app.py
@@ -0,0 +1,47 @@
+# Proyecto Flask que lee un archivo JSON y muestra su contenido en una página web 
+
+from flask import Flask, request # Importa Flask que es el framework web
+from pymongo import MongoClient # Importa MongoClient para interactuar con MongoDB
+
+app = Flask(__name__) # Crea una instancia de la aplicación Flask
+wsgi_app = app.wsgi_app # Exponer la aplicación WSGI para servidores compatibles con WSGI
+Cliente = MongoClient('mongodb://root:root@localhost:27050') # Conecta al servidor MongoDB en localhost y puerto 27017
+db = Cliente.Escuela # Selecciona la base de datos 'Escuela'
+Libros = db.Libros # Selecciona la colección 'Libros'
+
+@app.route('/') # Define la ruta para la URL raíz
+def hello(): # Define la función que se ejecuta cuando se accede a la ruta
+
+    salida = "<h1>Libros de Python</h1>"
+    salida += "<ul>"
+    
+    items=list(Libros.find())
+    for libro in items: # Itera sobre cada libro en la lista de libros
+        salida += "".join(f"<li>{libro['id']} - {libro['title']} - {libro['author']}</li>")
+        # Agrega cada libro a la salida en formato HTML
+    salida += "</ul>" # Cierra la lista HTML0
+    return salida # Devuelve la salida como respuesta HTTP
+@app.route('/libro',methods=["GET"]) 
+def api_libro():
+    salida=""
+    if "id" in request.args:
+        id = int(request.args["id"])
+    else:
+        return "No se ha proporcionado un identificador de libro"
+    items=list(Libros.find({"id":id}))
+    for libro in items:  # Itera sobre cada libro en la lista de libros
+   #     if libro["id"]== id:
+            salida += "".join(f"<li>{libro['id']} - {libro['isbn']} - {libro['title']} - {libro['author']}</li>")
+            continue   
+    return salida #falta validar si no encuentra el libro
+
+if __name__ == '__main__': # Si el script se ejecuta directamente, inicia el servidor de desarrollo
+ 
+
+    import os # Importa el módulo os para acceder a variables de entorno
+    HOST = os.environ.get('SERVER_HOST', 'localhost') # Obtiene el host del entorno o usa 'localhost' por defecto
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5000')) # Obtiene el puerto del entorno o usa 5000 por defecto
+    except ValueError:
+        PORT = 5000 # Si la conversión falla, usa 5000 por defecto
+    app.run(HOST, PORT) # Inicia el servidor de desarrollo en el host y puerto especificados
